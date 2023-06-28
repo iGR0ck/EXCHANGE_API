3618c8ce9c7076f87fb6c5d52ae753d8a03f0a79
igorpetushkov <igor.igorevich08@gmail.com>
2023-06-28 15:53:03 +0300

    main готовая программа для курса евро и дол к рублю

---
+++ api_test.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=RUB&amount=100"
+
+payload = {}
+headers= {
+  "apikey": "kd6L0Fya2eeMDxbG7uZToLAaDTJpCeTK"
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+status_code = response.status_code
+result = response.text
+
+print (status_code, result)
+
